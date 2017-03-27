c22372bdc12492176f039f743335897cc64afcaf
YISI QIU <arcsofia.yau@hotmail.com>
2017-03-27 17:17:58 -0500

    update

--- lab5.py
+++ lab5.py
@@ -1,4 +1,5 @@
 students = {}
+grades = []
 con = 'yes'
 while con != 'no':
     id = input("please enter student ID:")
@@ -7,5 +8,14 @@ while con != 'no':
     print(students.items())
     con = input("Do you have more to input, press yes to continue/ no to finish")
 
-number = input("how many assignments?")
-#        studnets[id][name].append()
+number = int(input("how many assignments?"))
+print(students)
+for id in students:
+    for i in range(number):
+        #if grades > 0 and grades < 100 :
+        grades.append(input("please enter score"+' '))
+        print(grades)
+        #else:
+        #   print("please input again")
+    students[id] = (grades) #append()
+    print(students)
